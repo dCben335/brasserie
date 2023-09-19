ed267a83799929feca2c8a6ea4737b348e70c88d
dCben335 <benoit.cabocel.335@gmail.com>
2023-09-19 13:00:58 +0200

    Create footer

--- components/molecules/Map/Map.tsx
+++ components/molecules/Map/Map.tsx
@@ -7,19 +7,21 @@ import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 
 export default function Map({}) {
+    const position = [40.8054, -74.0241] 
+
     return (
         <MapContainer 
-            center={[40.8054,-74.0241]} 
+            center={position} 
             zoom={14} 
             scrollWheelZoom={true}
             className='map'>
-                
+
             <TileLayer
                 attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
 
-            <Marker position={[40.8054,-74.0241]}>
+            <Marker position={position}>
                 <Popup>Hey ! you found me </Popup>
             </Marker>
     </MapContainer>
